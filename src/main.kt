402669425ab8e7d1ac809d4c15092cdf968ee8ce
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-07 23:15:40 +0300

    printed ok

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun twoOldestAges(ages: List<Int>): List<Int> {
+    return listOf(0, 0)
+}
+fun main(){
+   // twoOldestAges(listOf(2,3,,3,8,6,7))
+  //  twoOldestAges.sort()
+    //  println(twoOLdestAges)
+  //  println("Maximum: ${twoOldestAges.max()}")
+    start("OK")
+    var a= "OK"
+    println(a)
+
+}
+fun start(a:String):String{
+    return a
+
+
+}
+
+
